76a9268e2e9b1bfa1a31c05a7255931051d0c6c9
keysking <keysking.cn@gmail.com>
2025-03-09 20:27:48 +0800

    deploy添加压缩

--- deploy.ps1
+++ deploy.ps1
@@ -1,4 +1,8 @@
 npm run build
-scp -r ./build/* ubuntu@docs.baud-dance.com:~/docs
+Remove-Item .\build.zip
+Compress-Archive -Path .\build\* -DestinationPath .\build.zip
+scp -r ./build.zip ubuntu@docs.baud-dance.com:~/docs.zip
+# ssh ubuntu@docs.baud-dance.com "sudo rm -rf ~/docs/*"
+ssh ubuntu@docs.baud-dance.com "sudo unzip -o ~/docs.zip -d ~/docs"
 ssh ubuntu@docs.baud-dance.com "sudo rm -rf /opt/1panel/apps/openresty/openresty/www/sites/docs/index/*"
 ssh ubuntu@docs.baud-dance.com "sudo mv ~/docs/* /opt/1panel/apps/openresty/openresty/www/sites/docs/index"
