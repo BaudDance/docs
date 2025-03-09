0824661692e68580c194ad01b8da6c7f0cc6bbca
keysking <keysking.cn@gmail.com>
2025-03-09 21:52:24 +0800

    修复中文文件名失效问题

--- deploy.ps1
+++ deploy.ps1
@@ -1,8 +1,8 @@
 npm run build
 Remove-Item .\build.zip
-Compress-Archive -Path .\build\* -DestinationPath .\build.zip
-scp -r ./build.zip ubuntu@docs.baud-dance.com:~/docs.zip
-# ssh ubuntu@docs.baud-dance.com "sudo rm -rf ~/docs/*"
-ssh ubuntu@docs.baud-dance.com "sudo unzip -o ~/docs.zip -d ~/docs"
+tar -czvf build.tar.gz build/*
+scp -r ./build.tar.gz ubuntu@docs.baud-dance.com:~/docs.tar.gz
+ssh ubuntu@docs.baud-dance.com "sudo rm -rf ~/docs/*"
+ssh ubuntu@docs.baud-dance.com "sudo tar -zxvf docs.tar.gz -C ~/docs"
 ssh ubuntu@docs.baud-dance.com "sudo rm -rf /opt/1panel/apps/openresty/openresty/www/sites/docs/index/*"
-ssh ubuntu@docs.baud-dance.com "sudo mv ~/docs/* /opt/1panel/apps/openresty/openresty/www/sites/docs/index"
+ssh ubuntu@docs.baud-dance.com "sudo mv ~/docs/build/* /opt/1panel/apps/openresty/openresty/www/sites/docs/index"
