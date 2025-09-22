195fe5140fbafe97febda7a45521492837ff7fe5
keysking <keysking.cn@gmail.com>
2025-09-22 11:58:34 +0800

    更新上传网址

--- deploy.ps1
+++ deploy.ps1
@@ -1,8 +1,8 @@
 npm run build
 Remove-Item .\build.zip
 tar -czvf build.tar.gz build/*
-scp -r ./build.tar.gz ubuntu@docs.baud-dance.com:~/docs.tar.gz
-ssh ubuntu@docs.baud-dance.com "sudo rm -rf ~/docs/*"
-ssh ubuntu@docs.baud-dance.com "sudo tar -zxvf docs.tar.gz -C ~/docs"
-ssh ubuntu@docs.baud-dance.com "sudo rm -rf /opt/1panel/apps/openresty/openresty/www/sites/docs/index/*"
-ssh ubuntu@docs.baud-dance.com "sudo mv ~/docs/build/* /opt/1panel/apps/openresty/openresty/www/sites/docs/index"
+scp -r ./build.tar.gz ubuntu@ssh.baud-dance.com:~/docs.tar.gz
+ssh ubuntu@ssh.baud-dance.com "sudo rm -rf ~/docs/*"
+ssh ubuntu@ssh.baud-dance.com "sudo tar -zxvf docs.tar.gz -C ~/docs"
+ssh ubuntu@ssh.baud-dance.com "sudo rm -rf /opt/1panel/apps/openresty/openresty/www/sites/docs/index/*"
+ssh ubuntu@ssh.baud-dance.com "sudo mv ~/docs/build/* /opt/1panel/apps/openresty/openresty/www/sites/docs/index"
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 npm run build
-scp -r ./build/* ubuntu@docs.baud-dance.com:~/docs
-ssh ubuntu@docs.baud-dance.com "sudo rm -rf /opt/1panel/apps/openresty/openresty/www/sites/docs/index/*"
-ssh ubuntu@docs.baud-dance.com "sudo mv ~/docs/* /opt/1panel/apps/openresty/openresty/www/sites/docs/index"
+scp -r ./build/* ubuntu@ssh.baud-dance.com:~/docs
+ssh ubuntu@ssh.baud-dance.com "sudo rm -rf /opt/1panel/apps/openresty/openresty/www/sites/docs/index/*"
+ssh ubuntu@ssh.baud-dance.com "sudo mv ~/docs/* /opt/1panel/apps/openresty/openresty/www/sites/docs/index"
