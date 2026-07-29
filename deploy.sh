84a1bc94e78e05fc81e2fd07e3b6c2b4fa40d55c
keysking <keysking.cn@gmail.com>
2026-07-29 15:58:47 +0800

    优化Linux部署

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 npm run build
-rm -f .\build.zip
+rm -f build.tar.gz
 tar -czvf build.tar.gz build/*
-scp -r ./build.tar.gz ubuntu@ssh.baud-dance.com:~/docs.tar.gz
+scp ./build.tar.gz ubuntu@ssh.baud-dance.com:~/docs.tar.gz
 ssh ubuntu@ssh.baud-dance.com "sudo rm -rf ~/docs/*"
 ssh ubuntu@ssh.baud-dance.com "sudo tar -zxvf docs.tar.gz -C ~/docs"
 ssh ubuntu@ssh.baud-dance.com "sudo rm -rf /opt/1panel/apps/openresty/openresty/www/sites/docs/index/*"
